6764be4eef03391cff5add755b8ec4a3dc4151a6
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-03-14 15:11:27 +0600

    Add font style to responses

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
 const password = 'ILoveReact';
+const fontStyle = 'font-family: Arial, sans-serif;';
 
 app.get('/', (_req, res) => {
-  res.send('Home\n');
+  res.send(`<h1 style="${fontStyle}">Home</h1>\n`);
 });
 
 app.get('/Hello', (_req, res) => {
-  res.send('Hello!\n');
+  res.send(`<h1 style="${fontStyle}">Hello!</h1>\n`);
 });
 
 app.get('/encode/:message', (req, res) => {
   const encoded = Vigenere.Cipher(password).crypt(req.params.message);
-  res.send(`Encoded message: ${encoded}\n`);
+  res.send(`<h1 style="${fontStyle}">Encoded message: ${encoded}</h1>\n`);
 });
 
 app.get('/decode/:message', (req, res) => {
   const decoded = Vigenere.Decipher(password).crypt(req.params.message);
-  res.send(`Decoded message: ${decoded}\n`);
+  res.send(`<h1 style="${fontStyle}">Decoded message: ${decoded}</h1>\n`);
 });
 
 app.listen(port, () => {
